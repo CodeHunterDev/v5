70194d0746fce9ddfc6a717253ae6803fe68a500
Sandhj <135236046+Sandhj@users.noreply.github.com>
2023-07-23 21:36:57 +0700

    Update ssh-vpn.sh

--- tools/ssh-vpn.sh
+++ tools/ssh-vpn.sh
@@ -2,7 +2,6 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
-# Author  : Wings Premium VPN
 # (C) Copyright 2023
 # =========================================
 
